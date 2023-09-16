19a94e5d9190ea096ae6a31c89f00b86b4cc788e
abdelrahman Hamdy <abdoe317111@gmail.com>
2023-09-17 00:34:44 +0300

    ,

---
+++ E-Learning.BL/DTO/DeleteUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.BL;
+
+public class DeleteUserDto
+{
+
+    public string? UserId { get; set; }  
+    public string? AdminPassword { get; set; }   
+}
