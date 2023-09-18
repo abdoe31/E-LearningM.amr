7edbb53d6bdee164060c8320ff1921e3702c66a8
Abdelrahman Hamdy Abdelrehem Sayed <abdelrahman.hamdy@sha.edu.eg>
2023-09-18 16:01:50 +0200

    .

--- E-Learning.API/Controllers/QuizController.cs
+++ E-Learning.API/Controllers/QuizController.cs
@@ -2,6 +2,7 @@
 using E_Learning.DAL;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace E_Learning.API.Controllers
@@ -78,6 +79,38 @@ namespace E_Learning.API.Controllers
 
 
         }
+
+        [HttpPost("GetAllQAByQuiz/{Quizid}")]
+
+        public IActionResult StudentSolveQuiz( SolveQuizDto solveQuizDto )
+        
+        
+        {
+
+            int Grade = 0;
+List<UserAnswer> userAnswers = new List<UserAnswer>();
+            foreach (var R in solveQuizDto.userAnswerDtos)
+            {
+                var qustion = eLearningContext.Questions.FirstOrDefault(x => x.Id == R.QuestionId);
+                UserAnswer userAnswer = new UserAnswer { Answerid = R.AnswerID, QuestionId = qustion.Id, Right = R.AnswerID == qustion.RightAnswerid };
+                userAnswers.Add(userAnswer);
+            }
+            var quiz = eLearningContext.Quizes.Where(x => x.Id == solveQuizDto.Quizid).Include(x=>x.Questions).FirstOrDefault();
+            var userquiz = new UserQuiz {
+                Quizid = solveQuizDto.Quizid,  Studentid= solveQuizDto.Userid,
+                Start = DateTime.Now,
+                End = DateTime.Now.AddMinutes((int)quiz.Duration), UserAnswers= userAnswers, Grade= !userAnswers.IsNullOrEmpty()? userAnswers.Count(x=>x.Right==true ):0  
+            };
+
+            eLearningContext.UserQuizzes.Add(userquiz);
+
+            var states = eLearningContext.SaveChanges();
+            return Ok(new {  UserGrade= userquiz.Grade, colEfected= states });
+        }
+    
+    
+
+
 
     }
 }
