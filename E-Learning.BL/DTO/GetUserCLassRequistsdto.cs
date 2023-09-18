d09d70f89ccd50a66efd1ea57056a4acaa3f959f
abdelrahman Hamdy <abdoe317111@gmail.com>
2023-09-18 21:16:46 +0300

    .

--- E-Learning.BL/DTO/GetUserCLassRequistsdto.cs
+++ E-Learning.BL/DTO/GetUserCLassRequistsdto.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace E_Learning.BL.DTO
+namespace E_Learning.BL
 {
 
     public class GetCLassdto
     {
 
-        public string ClassName { get; set; }
+        public string? ClassName { get; set; }
 
-        public ICollection<GetUserCLassRequistsdto>   getUserCLassRequistsdtos = new List<GetUserCLassRequistsdto>();
+        public List<GetUserCLassRequistsdto>   getUserCLassRequistsdtos { get; set; } = new List<GetUserCLassRequistsdto>();
     }   
         public class GetUserCLassRequistsdto
     {
 
-        public int classid { get; set; }
-         public string Userid {  get; set; }
-        public string UserName {  get; set; }
+        public int? classid { get; set; }
+         public string? Userid {  get; set; }
+        public string? UserName {  get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ParentPhoneNumber { get; set; }
+
+
     }
 }
